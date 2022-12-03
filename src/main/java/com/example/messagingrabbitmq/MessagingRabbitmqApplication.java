1d67bede3e21bfdb7bc9bef2becb9d36d664fdf4
Maxim <maxim.geer@yandex.ru>
2022-12-03 16:45:56 +0500

    Multiple work

--- src/main/java/com/example/messagingrabbitmq/MessagingRabbitmqApplication.java
+++ src/main/java/com/example/messagingrabbitmq/MessagingRabbitmqApplication.java
@@ -1,9 +1,6 @@
 package com.example.messagingrabbitmq;
 
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
 import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
@@ -14,23 +11,24 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class MessagingRabbitmqApplication {
 
-	static final String topicExchangeName = "spring-boot-exchange";
+	//static final String topicExchangeName = "spring-boot-exchange"; + System.currentTimeMillis();
+	static final String fanoutExchangeName = "spring-boot-exchange";
 
-	static final String queueName = "spring-boot";
+	static final String queueName = "spring-boot" +  System.currentTimeMillis();
 
 	@Bean
 	Queue queue() {
-		return new Queue(queueName, false);
+		return new Queue(queueName, false, false, true);
 	}
 
 	@Bean
-	TopicExchange exchange() {
-		return new TopicExchange(topicExchangeName);
+	FanoutExchange exchange() {
+		return new FanoutExchange(fanoutExchangeName);
 	}
 
 	@Bean
-	Binding binding(Queue queue, TopicExchange exchange) {
-		return BindingBuilder.bind(queue).to(exchange).with("foo.bar.#");
+	Binding binding(Queue queue, FanoutExchange exchange) {
+		return BindingBuilder.bind(queue).to(exchange);//.with("foo.bar.#");
 	}
 
 	@Bean
--- src/main/java/com/example/messagingrabbitmq/Runner.java
+++ src/main/java/com/example/messagingrabbitmq/Runner.java
@@ -5,6 +5,7 @@ import java.util.concurrent.TimeUnit;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+import java.util.Scanner;
 
 @Component
 public class Runner implements CommandLineRunner {
@@ -20,8 +21,24 @@ public class Runner implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		System.out.println("Sending message...");
-		rabbitTemplate.convertAndSend(MessagingRabbitmqApplication.topicExchangeName, "foo.bar.baz", "Hello from RabbitMQ!");
+		//rabbitTemplate.convertAndSend(MessagingRabbitmqApplication.topicExchangeName, "foo.bar.baz", "Hello from RabbitMQ!");
+		rabbitTemplate.convertAndSend(MessagingRabbitmqApplication.fanoutExchangeName, "","Hello from RabbitMQ!");
+
 		receiver.getLatch().await(10000, TimeUnit.MILLISECONDS);
+
+		Scanner in = new Scanner(System.in);
+		while(true)
+		{
+			String msg = in.nextLine();
+			if(msg == "exit")
+			{
+				break;
+			}
+			//rabbitTemplate.convertAndSend(MessagingRabbitmqApplication.topicExchangeName, "foo.bar.baz", msg);
+			rabbitTemplate.convertAndSend(MessagingRabbitmqApplication.fanoutExchangeName, "", msg);
+
+		}
+
 	}
 
 }
